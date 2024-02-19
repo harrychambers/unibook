37ad4d534adcf9c86888ab9377c9ff3602b21143
harrychambers <harrychambers97@hotmail.co.uk>
2024-02-19 11:10:49 +0000

    lab complete

--- app.js
+++ app.js
@@ -5,13 +5,40 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
+
+    res.render("index");
+});
+
+app.get("/australia", async (req, res) => {
     let url = `http://universities.hipolabs.com/search?country=australia`;
 
     let response = await fetch(url); // use the fetch method
     let unis = await response.json(); // read response body and parse as JSON
-    console.table(unis[0].name);
-    let firstuni = unis[0];
-    res.render("index", {uni_data: firstuni});
+    res.render("country", {uni_data: unis});
+});
+
+app.get("/denmark", async (req, res) => {
+    let url = `http://universities.hipolabs.com/search?country=denmark`;
+
+    let response = await fetch(url); // use the fetch method
+    let unis = await response.json(); // read response body and parse as JSON
+    res.render("country", {uni_data: unis});
+});
+
+app.get("/france", async (req, res) => {
+    let url = `http://universities.hipolabs.com/search?country=france`;
+
+    let response = await fetch(url); // use the fetch method
+    let unis = await response.json(); // read response body and parse as JSON
+    res.render("country", {uni_data: unis});
+});
+
+app.get("/usa", async (req, res) => {
+    let url = `http://universities.hipolabs.com/search?country=united%20states`;
+
+    let response = await fetch(url); // use the fetch method
+    let unis = await response.json(); // read response body and parse as JSON
+    res.render("country", {uni_data: unis});
 });
 
 app.listen(3000, () => {
